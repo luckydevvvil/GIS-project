7a1c6ae13f19a210d04c196634e2df1dfc0fb0b7
Danil <mdr97@bk.ru>
2017-04-06 04:33:02 +0500

    Update vklogin.php

--- vklogin.php
+++ vklogin.php
@@ -6,26 +6,21 @@ $hash = $_GET["hash"];
 
 
 if ($_REQUEST['hash']==md5('5968477'.$uid.'CzPWUFZ4CoT2C1WtIy6V')) {
-  //доверяем вконтактику, и далее полагаем, что пользователь действительно авторизован там
-  //для учетных записей пользователей я решил выделить логины вида vk-********
   $result = mysql_query("SELECT id, random, password FROM tracker_users WHERE username = 'vk-$uid'");
   setcookie('uid','');
   setcookie('pass','');
   if (mysql_num_rows($result)) {
-    //пользователь авторизован, просто пересоздадим куки
     $user = mysql_fetch_assoc($result);
     mysql_query("UPDATE tracker_users SET name = '$name' WHERE username = 'vk-$uid' LIMIT 1");
     setcookie('pass',md5($user['random'].$user['password'].$user['random']));
     setcookie('uid',$user['id']);
   } else {
-    //добавим запись в таблицу пользователей
     $random = mt_rand(100000,999999);
     $pwd = $uid . 'verysecretlonglongword-';
     $pid=md5(uniqid(rand(),true));
     mysql_query("INSERT INTO tracker_users 
       (username, name, password, random, id_level, email, style, language, flag, joined, lastconnect, pid, time_offset) VALUES 
       ('vk-$uid', '$name', '" . md5($pwd) . "', $random, 3, '', 5, 7, 0, NOW(), NOW(),'$pid', '0')");
-    //вставили строчку, теперь создадим куки и перебросим на другую страницу
     setcookie('pass',md5($random.md5($pwd).$random));
     setcookie('uid',mysql_insert_id());
   }
